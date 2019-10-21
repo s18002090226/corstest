f57e5218ab3ba4e2b905004f80ee29bbe442223c
admin <1313@abc.com>
2019-10-21 09:13:33 +0800

    test

--- src/main/java/cn/wzvtc/soft/TestController.java
+++ src/main/java/cn/wzvtc/soft/TestController.java
@@ -12,6 +12,7 @@ import java.util.Map;
 @RestController("/" )
 public class TestController {
 
+
     @RequestMapping(value="/data.json",method={RequestMethod.GET})
     public Map<String,String> bookById(){
         Map<String,String> resultMap=new HashMap<>();
